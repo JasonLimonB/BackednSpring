f652fa9415a1efb8cc1cb1c1f7f703bff77986fe
Jason <jason.limon@outlook.com>
2020-06-15 19:56:13 -0500

    My project in Spring

---
+++ src/main/java/com/limoncode/LimoncodeApplication.java
@@ -0,0 +1,13 @@
+package com.limoncode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimoncodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimoncodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/limoncode/LimoncodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.limoncode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimoncodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
